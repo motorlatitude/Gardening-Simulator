887b0738663f3b39247902f41591af1b977cf858
Lennart Hase <1834270+motorlatitude@users.noreply.github.com>
2022-09-20 21:06:09 +0100

    Shift running movement and fixed going through wall bug
    Fixed that you could glitch through a wall by turning your camera 180deg. Also added faster player movement speed, modifier can also be set in Unity attributes.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public Collider playerCollider;
     public float mouseSensitivity = 2.0f;
     public float playerSpeed = 2.0f;
+    public float playerSpeedModifier = 1.5f;
     private Camera currentCamera;
     private CharacterController controller;
     // Start is called before the first frame update
@@ -14,6 +15,8 @@ public class Player : MonoBehaviour
     {
         controller = gameObject.AddComponent<CharacterController>();
         currentCamera = Camera.main;
+        controller.center = playerCollider.bounds.center;
+        controller.enableOverlapRecovery = true;
         // Should be scaled to fit character model
         controller.height = 0.1f;
         controller.radius = 0.1f;
@@ -28,19 +31,25 @@ public class Player : MonoBehaviour
         controller.transform.RotateAround(playerCollider.bounds.center, Vector3.up, h);
         currentCamera.transform.RotateAround(playerCollider.bounds.center, currentCamera.transform.right, -v);
 
+        float modifiedSpeed = playerSpeed;
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            modifiedSpeed = playerSpeed*playerSpeedModifier;
+        }
+
         if (Input.GetKey(KeyCode.W)){
-            controller.Move(transform.forward * Time.deltaTime * playerSpeed);
+            controller.Move(transform.forward * Time.deltaTime * modifiedSpeed);
         }
 
         if (Input.GetKey(KeyCode.S)){
-            controller.Move(-transform.forward * Time.deltaTime * playerSpeed);
+            controller.Move(-transform.forward * Time.deltaTime * modifiedSpeed);
         }
 
         if (Input.GetKey(KeyCode.D)){
             Vector3 rotated = Quaternion.AngleAxis(90, Vector3.up) * transform.forward;
             controller.Move(rotated * Time.deltaTime * playerSpeed);
         }
-        
+
         if (Input.GetKey(KeyCode.A)){
             Vector3 rotated = Quaternion.AngleAxis(-90, Vector3.up) * transform.forward;
             controller.Move(rotated * Time.deltaTime * playerSpeed);
