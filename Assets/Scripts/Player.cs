c87916fba6767360a85913305eb871c722e2868c
Lennart Hase <1834270+motorlatitude@users.noreply.github.com>
2022-09-20 16:04:01 +0100

    Improved Character Movement
    Character can now move freely and moves in the direction the camera is facing. Mouse also turns camera now. There are mouseSensitivity as well as character speed attributes that are editable as well.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Collider playerCollider;
+    public float mouseSensitivity = 2.0f;
+    public float playerSpeed = 2.0f;
+    private Camera currentCamera;
     private CharacterController controller;
     // Start is called before the first frame update
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
+        currentCamera = Camera.main;
         // Should be scaled to fit character model
         controller.height = 0.1f;
         controller.radius = 0.1f;
@@ -17,8 +22,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * 2.0f);
+        float h = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float v = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        //controller.transform.Rotate(0, h, 0);
+        controller.transform.RotateAround(playerCollider.bounds.center, Vector3.up, h);
+        currentCamera.transform.RotateAround(playerCollider.bounds.center, currentCamera.transform.right, -v);
+
+        if (Input.GetKey(KeyCode.W)){
+            controller.Move(transform.forward * Time.deltaTime * playerSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S)){
+            controller.Move(-transform.forward * Time.deltaTime * playerSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D)){
+            Vector3 rotated = Quaternion.AngleAxis(90, Vector3.up) * transform.forward;
+            controller.Move(rotated * Time.deltaTime * playerSpeed);
+        }
+        
+        if (Input.GetKey(KeyCode.A)){
+            Vector3 rotated = Quaternion.AngleAxis(-90, Vector3.up) * transform.forward;
+            controller.Move(rotated * Time.deltaTime * playerSpeed);
+        }
 
     }
 }
